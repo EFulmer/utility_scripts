77236c9aa8399e217e0ad4bd26832c740f195b08
EFulmer <EFulmer@gmail.com>
2013-10-15 21:52:36 -0400

    new python project script added

---
+++ new_python_project.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    # read in command line args; separated by spaces, each one is the name of
+    # a new Python project.
+    
+    # for each arg, create the project structure; this includes:
+    # making the top-level directory, should have same name as the arg
+    # making top dir's subdirs; proj_name (for source code), tests, doc
+    # creating a README in the top directory
+    # initializing a git repo
+    # possibly in the future setting up a virtualenv for it?
+    
+    # os.makedirs()
