84eae4f1d361993495facbd2fd207b74eb74eb8c
Eric Fulmer <EFulmer@gmail.com>
2013-11-23 14:32:39 -0500

    photo archiver uses default args for path and archive name now

--- update_photos_archive.py
+++ update_photos_archive.py
@@ -1,43 +1,46 @@
+import os
 import sys
 import tarfile
 
-# tarfile.open(mode='w:gz') for gzip
-# use fileobj kwarg if there's an already-opened file object
-# otherwise use name kwarg
 
-PHOTO_FOLDER = '/Users/eric/Dropbox/Camera Uploads'
-OUTPUT_FOLDER = '/Users/eric/Dropbox/Camera Uploads'
-ARCHIVE_NAME = ''
+IMAGE_DIR = '/Users/eric/Dropbox/Camera Uploads'
+ARCHIVE_FILE = '/Users/eric/Dropbox/Camera Uploads/photo_archive.tar.gz'
 IMAGE_FILE_EXTS = ('.bmp', '.gif', '.jpeg', '.jpg', '.png', )
+
 
 def is_image_file(f):
     return os.path.splitext(f)[1] in IMAGE_FILE_EXTS
 
-def get_image_files(folder):
-    # get list of files in folder
-    # loop over list of files, add each image file to list of img files
+
+def get_image_files(folder=IMAGE_DIR):
     fldr_contents = os.listdir(path=folder)
     img_files = []
     for f in fldr_contents:
         if is_image_file(f):
-            img_files.append(f)
+            img_files.append(os.path.join(folder, f))
+    return img_files
     # open up archive file
     # check for existence of any of these image files in the archive file (how?)
     # add new files to archive file
 
 
-def archive_image_files(img_files, archive_name, mode='w:gz'):
+def archive_image_files(img_files, archive_name=ARCHIVE_FILE, mode='w:gz'):
     with tarfile.open(archive_name, mode) as tar:
-        for img in img_files:
-            tar.add(img) 
+        for img in img_files: 
+            tar.add(img)
 
 
 def main():
-    try:
-        img_folder, archive_name = sys.argv[1:]
-        
-    except ValueError as v:
-        print('Expected image folder and archive name as args, '
+    if len(sys.argv) == 1:
+        images = get_image_files()
+        archive_image_files(images)
+    else:
+        try:
+            img_folder, archive_name = sys.argv[1:]
+            images = get_image_files(img_folder)
+            archive_image_files(images, archive_name)
+        except ValueError as v:
+            print('Expected image folder and archive name as args, '
                 'got {0}'.format(v.args()))
 
 
