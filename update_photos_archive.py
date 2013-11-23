62d6871b9126715c6458ef15fe218d577738bde2
Eric Fulmer <EFulmer@gmail.com>
2013-11-23 14:43:16 -0500

    docstrings!

--- update_photos_archive.py
+++ update_photos_archive.py
@@ -9,22 +9,28 @@ IMAGE_FILE_EXTS = ('.bmp', '.gif', '.jpeg', '.jpg', '.png', )
 
 
 def is_image_file(f):
+    """True if f is an image file. Checked by investigating f's extension."""
     return os.path.splitext(f)[1] in IMAGE_FILE_EXTS
 
 
 def get_image_files(folder=IMAGE_DIR):
+    """Return a list of all image files in folder."""
     fldr_contents = os.listdir(path=folder)
-    img_files = [ f for f in os.listdir(path=folder) if is_image_file(f) ]
-    #for f in fldr_contents:
-    #    if is_image_file(f):
-    #        img_files.append(os.path.join(folder, f))
+    img_files = [ os.path.join(folder, f) for f in os.listdir(path=folder) 
+                  if is_image_file(f) ]
     return img_files
 
 
-def archive_image_files(img_files, archive_name=ARCHIVE_FILE, mode='w:gz'):
+def archive_image_files(img_files, archive_name=ARCHIVE_FILE, mode='w:gz' 
+                        delete_after_archive=False):
+    # TODO handle existing archives; either rename archive_name if it
+    # exists or somehow make a new archive and merge it with the 
+    # existing one.
     with tarfile.open(archive_name, mode) as tar:
         for img in img_files: 
             tar.add(img)
+            if delete_after_archive:
+                os.remove(img)
 
 
 def main():
