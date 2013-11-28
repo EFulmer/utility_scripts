308c15547090a02e5d4124a0628eccbac03ceb8e
Eric Fulmer <EFulmer@gmail.com>
2013-11-28 02:59:51 -0500

    basic working version of backup_configs

--- backup_configs.py
+++ backup_configs.py
@@ -19,37 +19,44 @@ def newer_version(f, backup):
     f_stat = os.stat(f)
     try:
         b_stat = os.stat(backup)
-        return f_stat.st_ctime > b_stat.ctime
+        return f_stat.st_ctime > b_stat.st_ctime
     except OSError:
         # if file hasn't been backed up before, True by default
         return True
 
 
 def backup(f, backup_loc):
+    """
+    Back up file f to backup_loc. If f is a folder instead, 
+    back its contents up to backup_loc.
+    """
     # check what f is:
     # if f is a file, just do copy(f, os.path.join(f, backup_loc)
     # otherwise, os.walk over the directory and move each of the files.
     if os.path.isfile(f):
-        copy(f, os.path.join(f, backup_loc))
+        shutil.copy(f, os.path.join(f, backup_loc))
     else:
         dir_tree = os.walk(f)
-        
-        
+        # TODO this is ugly. make it nicer.
+        for item in dir_tree:
+            for fil in item[2]:
+                backup(fil, backup_loc) 
+            for fld in item[1]:
+                backup(fld, backup_loc)
 
 
-def backup_configs(configs, backup_loc):
+def get_updated_cfgs(configs, backup_loc):
     updated_cfgs = [ f for f in configs 
                      if newer_version(f, backup_loc + os.path.split(f)[1]) ] # if this doesn't work try adding os.path.isfile(f) as another condition
-    # map(copyfile/dir/whatever, config_files_that_have_been_updated)
-    # TODO handle new directories being added to CONFIG_FILES
-    print(updated_cfgs)
+    return updated_cfgs
 
 
 def main():
-    # TODO: move lambda into get_home function
-    cfgs = map(get_home, CONFIG_FILES)
-    bkup = map(get_home, BACKUP_FOLDER)
-    backup_configs(cfgs, bkup)
+    cfgs = list(map(get_home, CONFIG_FILES))
+    backup_loc = get_home(BACKUP_FOLDER)
+    files_to_bkup = get_updated_cfgs(cfgs, backup_loc)
+    for f in files_to_bkup:
+        backup(f, backup_loc)
 
 
 if __name__ == '__main__':
