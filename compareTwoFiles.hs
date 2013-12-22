5568c01b8388a5be4c0885ecf350c24385f74cd1
Eric Fulmer <EFulmer@gmail.com>
2013-12-22 02:43:17 -0500

    ported file comparison fn to Haskell

---
+++ compareTwoFiles.hs
@@ -0,0 +1,21 @@
+import qualified Crypto.Hash.SHA256 as SHA256
+import qualified Data.ByteString as B
+import System.Environment
+
+fileHash :: B.ByteString -> B.ByteString
+fileHash bs = go SHA256.init bs
+    where 
+        go ctx bs 
+            | bs == B.empty = SHA256.finalize ctx
+            | otherwise = go (SHA256.update ctx start) rest
+                    where 
+                        start = B.take 8 bs
+                        rest = B.drop 8 bs
+
+main = do 
+    args <- getArgs
+    let fName1 = args !! 0 
+        fName2 = args !! 1
+    f1 <- B.readFile fName1
+    f2 <- B.readFile fName2
+    print $ (fileHash f1) == (fileHash f2)
